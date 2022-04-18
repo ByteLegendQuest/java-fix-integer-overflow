f765dc3676ffdb0a090fad9421b956f47e55a093
chinahzh <2987140244@qq.com>
2022-04-18 00:21:40 +0000

    Update at 2022-04-18T00:21:40.725925Z

--- src/main/java/com/bytelegend/IntegerOverflow.java
+++ src/main/java/com/bytelegend/IntegerOverflow.java
@@ -2,8 +2,8 @@ package com.bytelegend;
 
 public class IntegerOverflow {
     public static void main(String[] args) {
-        int oneBillion = 1_000_000_000;
-        int i = 0;
+        unsigned int oneBillion = 1_000_000_000;
+        unsigned int i = 0;
 
         i = i + oneBillion;
         System.out.println("i=" + i);
