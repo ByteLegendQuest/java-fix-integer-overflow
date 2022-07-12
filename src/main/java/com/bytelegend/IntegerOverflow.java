875292b6b14259c83dbd73c308c29792351c580b
chesterbachelor <kawalerbartosz@gmail.com>
2022-07-12 22:12:29 +0000

    Update at 2022-07-12T22:12:29.044662Z

--- src/main/java/com/bytelegend/IntegerOverflow.java
+++ src/main/java/com/bytelegend/IntegerOverflow.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class IntegerOverflow {
     public static void main(String[] args) {
-        int oneBillion = 1_000_000_000;
+        int oneBillion = 1000000000;
         int i = 0;
 
         i = i + oneBillion;
