26c8529d1df2e73882b1eb1cd0c0209910e92fea
TOHSAKA-RINN <1911872724@qq.com>
2022-07-22 13:11:45 +0000

    Update at 2022-07-22T13:11:45.035837Z

--- src/main/java/com/bytelegend/IntegerOverflow.java
+++ src/main/java/com/bytelegend/IntegerOverflow.java
@@ -2,8 +2,8 @@ package com.bytelegend;
 
 public class IntegerOverflow {
     public static void main(String[] args) {
-        Long oneBillion = 1_000_000_000;
-        Long i = 0;
+        long oneBillion = 1_000_000_000;
+        long i = 0;
 
         i = i + oneBillion;
         System.out.println("i=" + i);
