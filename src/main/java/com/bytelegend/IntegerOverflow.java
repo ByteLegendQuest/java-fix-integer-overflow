188af60191442b173971c0c0d314639efb5fa3db
lhhhhhhhhhhhhhh <lihan881212@gmail.com>
2022-09-22 07:02:36 +0000

    Update at 2022-09-22T07:02:36.058987Z

--- src/main/java/com/bytelegend/IntegerOverflow.java
+++ src/main/java/com/bytelegend/IntegerOverflow.java
@@ -9,7 +9,7 @@ public class IntegerOverflow {
         System.out.println("i=" + i);
         i = i + oneBillion;
         System.out.println("i=" + i);
-        long l = i + oneBillion;
+        long l = (long)i + oneBillion;
         System.out.println("i=" + l);
     }
 }
