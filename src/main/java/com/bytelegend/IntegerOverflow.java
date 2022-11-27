d66571bff009cbf6c86ed461a710883fa0cbed97
pamaomaoa <544060246@qq.com>
2022-11-27 13:54:08 +0000

    Update at 2022-11-27T13:54:08.654207Z

--- src/main/java/com/bytelegend/IntegerOverflow.java
+++ src/main/java/com/bytelegend/IntegerOverflow.java
@@ -2,14 +2,14 @@ package com.bytelegend;
 
 public class IntegerOverflow {
     public static void main(String[] args) {
-        int oneBillion = 1_000_000_000;
-        int i = 0;
+        BigInteger oneBillion = new BigInteger("1000000000");
+        BigInteger i = BigInteger.ZERO;
 
-        i = i + oneBillion;
+        i = i.add(oneBillion);
         System.out.println("i=" + i);
-        i = i + oneBillion;
+        i = i.add(oneBillion);
         System.out.println("i=" + i);
-        i = i + oneBillion;
+        i = i.add(oneBillion);
         System.out.println("i=" + i);
     }
 }
