66bb2310c60f59013c72f60cd794e8099e63c9af
andyliukun <744059787@qq.com>
2023-01-29 09:56:31 +0000

    Update at 2023-01-29T09:56:31.972953Z

--- src/main/java/com/bytelegend/IntegerOverflow.java
+++ src/main/java/com/bytelegend/IntegerOverflow.java
@@ -3,7 +3,7 @@ package com.bytelegend;
 public class IntegerOverflow {
     public static void main(String[] args) {
         long oneBillion = 1_000_000_000;
-        int i = 0;
+        long i = 0;
 
         i = i + oneBillion;
         System.out.println("i=" + i);
